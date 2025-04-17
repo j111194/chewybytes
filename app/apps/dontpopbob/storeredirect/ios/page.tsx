9366379ad29f8b185511c5c3af2ebc0216aa2a10
Joris (Private) <jorisprivate@Joriss-MacBook-Pro.local>
2025-04-17 20:05:01 +0200

    Redirects

---
+++ app/apps/dontpopbob/storeredirect/ios/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { useEffect } from "react"
+
+export default function GamePage() {
+  useEffect(() => {
+    window.location.href = "https://apps.apple.com/us/app/dont-pop-bob/id6447240871"
+  }, [])
+
+  return null
+}
